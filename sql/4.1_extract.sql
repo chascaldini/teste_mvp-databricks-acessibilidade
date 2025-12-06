fb151ebb5946ac3c0e4d7a0d6aa4a6b74171586c
Charlyne Scaldini <chascaldini@gmail.com>
2025-12-06 01:49:02 -0300

    Update and rename extract.sql to 4.1_extract.sql
    Comandos utilizados para inspeção da tabela bruta

---
+++ sql/4.1_extract.sql
@@ -0,0 +1,11 @@
+-- Preview da tabela bruta
+SELECT *
+FROM consultas_fbn_bruta
+LIMIT 20;
+
+-- Schema da tabela bruta
+DESCRIBE TABLE consultas_fbn_bruta;
+
+-- Contagem de registros
+SELECT COUNT(*)
+FROM consultas_fbn_bruta;
